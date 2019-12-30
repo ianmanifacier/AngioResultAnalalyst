2acc13248aabb473d32257184e62d9c9ca7af044
Ian Manifacier <ianmanifacier@gmail.com>
2019-12-30 14:00:56 +0100

    added nbExtremities plot analysis

--- result_reader/plotmigrationpatterns.py
+++ result_reader/plotmigrationpatterns.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 import numpy as np
 import csv
 from math import sqrt
+from math import floor
 
 class CellSimulationResult:
     """ CellSimulationResult is an object that contains all the results of a simulation
@@ -22,6 +23,7 @@ class CellSimulationResult:
         self.y = np.empty([1000, ], dtype=float)
         self.area = np.empty([1000, ], dtype=float)
         self.nbSteps = 0
+        self.nbExtremities = "not defined"
     def distance(self):
         x = self.x
         y = self.y
@@ -36,6 +38,7 @@ class CellSimulationResult:
         self.area[self.nbSteps] = area
     def nextStep(self):
         self.nbSteps += 1
+
 
 
 def historgram(x):
@@ -70,15 +73,44 @@ def load_results(filename = 'results.csv'):
                 myCells[simKey] = CellSimulationResult(simKey)
                 myCells[simKey].K_stiffness_central = float(row[1]) # K_stiffness_central
                 myCells[simKey].K_stiffness_edge = float(row[2]) # K_stiffness_edge
+                myCells[simKey].nbExtremities = int(row[7])
             else:
                 myCells[simKey].nextStep()
             myCells[simKey].append_t( float(row[3]) ) # t (time)
             myCells[simKey].append_x( float(row[4]) ) # x
             myCells[simKey].append_y( float(row[5]) ) # y
             myCells[simKey].append_area( float(row[6]) ) # area
+
             line_count += 1
         return myCells
 
+##############################################################
+################# Ploting tools ##############################
+##############################################################
 
+def assign_subplot(rows, cols, nvalue):
+    """Return assigns the row and column of the element to be ploted based on it's nvalue.
+        
+    Inputs:
+        - rows (int) : number of rows of subplots
+        - columns (int) : number of columns of suplot
+        - nvalue (int) : number assigned to the category
+    Output:
+        - row (int)
+        - col (int)
+    """
 
+    if nvalue > (rows*cols):
+        print('value out of bounds')
+        exit()
+        # raise Error('value out of bounds')
+    else:
+        col = nvalue % cols + 1
+        row = floor(nvalue/cols) + 1
+        return row, col
 
+def assign_subplot_row(cols, nvalue):
+    return floor(nvalue/cols) + 1
+
+def assign_subplot_col(cols, nvalue):
+    return nvalue % cols + 1
--- results_parameter_analysis_nbextremities.py
+++ results_parameter_analysis_nbextremities.py
@@ -11,23 +11,25 @@ from math import floor
 # local library imports
 from result_reader import plotmigrationpatterns as myplt
 
+
+# Loading data : opening the csv file that contains the data
 filename = os.path.join(os.getcwd(),'..\\results_parameter_analysis_nbextremities.csv')
 print("uploading file: {}".format(filename))
 myCells = myplt.load_results(filename)
 
-
+# Ploting: we create a 15 subplots to visually compare the migration paterns generated based on the number of extremities
 rows = 4
 cols = 4
 subplot_titles=("2 extremities", "3 extremities", "4 extremities", "5 extremities", "6 extremities", "7 extremities",
                 "8 extremities", "9 extremities", "10 extremities", "11 extremities", "12 extremities", "13 extremities",
                 "14 extremities", "15 extremities", "16 extremities")
 
-fig2 = make_subplots(rows=rows, cols=cols, subplot_titles=subplot_titles)
+fig2 = make_subplots(rows=rows, cols=cols, subplot_titles=subplot_titles) #  ( how to define subplots: https://plot.ly/python/subplots/ )
 
 
 
 for cell in myCells:
-    nvalue = myCells[cell].nbExtremities - 2 
+    nvalue = myCells[cell].nbExtremities - 2 # we subtract 2 because we start with 2 extremities
     row, col = myplt.assign_subplot(rows, cols, nvalue)
     nbSteps = myCells[cell].nbSteps
     fig2.add_trace(go.Scatter(
